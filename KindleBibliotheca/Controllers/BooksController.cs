b31a90a59db55044724dd02f6789e0a0ce2fc120
alexa21e <pirvuletu.e.alexandra@gmail.com>
2023-12-05 19:42:33 +0200

    Add books controller

---
+++ KindleBibliotheca/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KindleBibliotheca.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BibliothecaContext _context;
+        public BooksController(BibliothecaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            var books = await _context.Books.ToListAsync();
+            return books;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+    }
+}
