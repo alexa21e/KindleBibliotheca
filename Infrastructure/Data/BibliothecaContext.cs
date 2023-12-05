1b6dbc9d3fdb6a8fcc620fc764e0984ea174b4f1
alexa21e <pirvuletu.e.alexandra@gmail.com>
2023-12-05 19:33:21 +0200

    Add context and Entity Framework

---
+++ Infrastructure/Data/BibliothecaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class BibliothecaContext : DbContext
+    {
+        public BibliothecaContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
