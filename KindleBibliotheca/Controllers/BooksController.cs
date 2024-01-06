746e547eda03741ba616024293707123f4d5a385
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-01-06 17:47:33 +0200

    Add Swagger for documenting the API

--- KindleBibliotheca/Controllers/BooksController.cs
+++ KindleBibliotheca/Controllers/BooksController.cs
@@ -5,9 +5,11 @@ using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
 using KindleBibliotheca.DTOs;
+using KindleBibliotheca.Errors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace KindleBibliotheca.Controllers
 {
@@ -26,6 +28,9 @@ namespace KindleBibliotheca.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [SwaggerOperation(Summary = "Return list of all books")]
         public async Task<ActionResult<List<BookToReturn>>> GetBooks()
         {
             var spec = new BooksWithSeriesSpecifications();
@@ -34,14 +39,24 @@ namespace KindleBibliotheca.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
+        [SwaggerOperation(Summary = "Return book by id")]
         public async Task<ActionResult<BookToReturn>> GetBook(Guid id)
         {
             var spec = new BooksWithSeriesSpecifications(id);
             var book = await _booksRepo.GetEntityWithSpec(spec);
+            if (book == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
             return _mapper.Map<Book, BookToReturn>(book);
         }
 
         [HttpGet("series")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [SwaggerOperation(Summary = "Return list of all series")]
         public async Task<ActionResult<List<Series>>> GetSeries()
         {
             return Ok(await _seriesRepo.ListAllAsync());
