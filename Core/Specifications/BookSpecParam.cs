b0721b111996ac84d0319819ea2411e8f91ae126
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-01-07 16:22:52 +0200

    Add CORS

--- Core/Specifications/BookSpecParam.cs
+++ Core/Specifications/BookSpecParam.cs
@@ -15,7 +15,7 @@
         public string? Sort { get; set; }
 
         private string _search;
-        public string Search
+        public string? Search
         {
             get => _search;
             set => _search = value.ToLower();
