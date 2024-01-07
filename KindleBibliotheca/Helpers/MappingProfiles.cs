8b68044beb3d03be698d607fadd7c09ebefd5988
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-01-07 17:03:16 +0200

    Adding Author exception in the mapping profiles

--- KindleBibliotheca/Helpers/MappingProfiles.cs
+++ KindleBibliotheca/Helpers/MappingProfiles.cs
@@ -10,6 +10,7 @@ namespace KindleBibliotheca.Helpers
         {
             CreateMap<Book, BookToReturn>()
                 .ForMember(d => d.Series, o => o.MapFrom(s => s.Series.Name))
+                .ForMember(d => d.Author, o => o.MapFrom(s => s.Author.Name))
                 .ForMember(d => d.CoverUrl, o => o.MapFrom<BookUrlResolver>());
         }
     }
