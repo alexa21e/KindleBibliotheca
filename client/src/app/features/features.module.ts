4c57c800d3663a8ccbf6551bd6a12424c52b99f9
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-01-19 19:18:44 +0200

    Implement UI for home page

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BookComponent
   ],
   imports: [
     CommonModule
