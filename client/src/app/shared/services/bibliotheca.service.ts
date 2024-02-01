88e49b16b5f22c6716b23375f5c9ba7d94b006b1
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-02-01 18:52:55 +0200

    Add endpoints for series & authors

--- client/src/app/shared/services/bibliotheca.service.ts
+++ client/src/app/shared/services/bibliotheca.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Pagination } from "../models/pagination";
 import { Book } from "../models/book";
+import { Series } from "../models/series";
+import { Author } from "../models/author";
 
 @Injectable({
     providedIn: 'root' 
@@ -14,5 +16,13 @@ export class BibliothecaService{
 
     getBooks(){
         return this.http.get<Pagination<Book[]>>(this.baseUrl + 'books?pageSize=50');
+    }
+
+    getSeries(){
+        return this.http.get<Series[]>(this.baseUrl + 'books/series');
+    }
+
+    getAuthors(){
+        return this.http.get<Author[]>(this.baseUrl + 'books/authors');
     }
 }
