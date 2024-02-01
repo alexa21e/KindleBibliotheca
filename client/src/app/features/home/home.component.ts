f4e02f9f962aa2546a9897a34ed547bad71a6a24
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-02-01 18:53:52 +0200

    Update home page with actual series and authors

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Author } from 'src/app/shared/models/author';
 import { Book } from 'src/app/shared/models/book';
+import { Series } from 'src/app/shared/models/series';
 import { BibliothecaService } from 'src/app/shared/services/bibliotheca.service';
 
 @Component({
@@ -9,14 +11,35 @@ import { BibliothecaService } from 'src/app/shared/services/bibliotheca.service'
 })
 export class HomeComponent implements OnInit{
   books: Book[] = [];
+  series: Series[] = [];
+  authors: Author[] = [];
 
   constructor( private bibliothecaService: BibliothecaService){}
   
   ngOnInit(): void {
+    this.getProducts();
+    this.getAuthors();
+    this.getSeries();
+  }
+
+  getProducts(){
     this.bibliothecaService.getBooks().subscribe({
       next: response => this.books = response.data,
-      error: error => console.log(error),
-      
+      error: error => console.log(error)
+    });
+  }
+
+  getSeries(){
+    this.bibliothecaService.getSeries().subscribe({
+      next: response => this.series = response,
+      error: error => console.log(error)
+    });
+  }
+
+  getAuthors(){
+    this.bibliothecaService.getAuthors().subscribe({
+      next: response => this.authors = response,
+      error: error => console.log(error)
     });
   }
 
