6943a65a40ca7de0ba71336280919a8a9e20eeab
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-02-22 23:33:16 +0200

    Creating the routes and the necessary components

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { BookComponent } from './book/book.component';
 import { SharedModule } from '../shared/shared.module';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { LibraryComponent } from './library/library.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    BookComponent
+    BookComponent,
+    BookDetailsComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+    LibraryComponent
   ],
   imports: [
     CommonModule,
