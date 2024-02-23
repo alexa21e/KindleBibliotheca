ad9fd92406985340fa29059e872e7a416b584d96
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-02-23 14:30:43 +0200

    Update Book DTO and Book model for accessing author id and series id

--- client/src/app/shared/models/book.ts
+++ client/src/app/shared/models/book.ts
@@ -1,11 +1,13 @@
 export interface Book {
     id: string
     title: string
+    authorId: string
     author: string
     publishingDate: string
     rating: number
     genre: number
     publishingHouse: string
+    seriesId?:string
     series?: string
     seriesPlace?: number
     pagesNumber: number
